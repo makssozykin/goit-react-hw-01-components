62208dcae87589290e679799a224d4f4c0a07a8d
makssozykin <mvsozykin@gmail.com>
2023-09-18 17:02:20 +0300

    task2 done render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
-import user from '../user.json';
+import user from '../data/user.json';
 import { Profile } from 'components/Profile/Profile';
+
+import data from '../data/data.json';
+import { Statistics } from 'components/Statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="UPLOAD STATS" stats={data} />
+    </div>
   );
 };
